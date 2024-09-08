44447eba263032f3d0a23dc51eb736e1199a3c19
ritik jha <coolritikjha46@gmail.com>
2024-09-08 20:24:16 +0530

    one more round of testing

--- client/src/components/admin/orders/AdminOrdersTable.js
+++ client/src/components/admin/orders/AdminOrdersTable.js
@@ -5,7 +5,6 @@ import Message from "../../Message"
 import { Spin, Table, Tag } from "antd"
 import { Link } from "react-router-dom"
 
-
 const AdminOrdersTable = () => {
 
     const orderList = useSelector((state) => state.orderList)
